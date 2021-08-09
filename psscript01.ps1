b2d113b214331f9a160d9a823275104d4061b183
anushabc <66515706+anushabc@users.noreply.github.com>
2021-08-09 13:54:53 +0530

    Update psscript01.ps1

--- psscript01.ps1
+++ psscript01.ps1
@@ -35,11 +35,9 @@ InstallCloudLabsShadow $ODLID $InstallCloudLabsShadow
 CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID $DeploymentID $azuserobjectid
 InstallVSCode
 choco install azure-data-studio
-InstallPowerBIDesktop
 choco install dotnetcore-sdk
 choco install azure-functions-core-tools
 InstallAzCLI
-choco install powerbi -y -force
 sleep 10
 
 #Shortcut for Azure Data Studio
@@ -48,24 +46,6 @@ $Shortcut = $WshShell.CreateShortcut("C:\Users\Public\Desktop\Azure Studio.lnk")
 $Shortcut.TargetPath = """C:\Program Files\Azure Data Studio\azuredatastudio.exe"""
 $Shortcut.Save()
 
-#Download lab files
-New-Item -ItemType directory -Path C:\MCW
-$WebClient = New-Object System.Net.WebClient
-#Download lab files
-$WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://github.com/microsoft/MCW-Innovate-and-modernize-apps-with-Data-and-AI/archive/main.zip","C:\MCW\MCW-Innovate-and-modernize-apps-with-Data-and-AI.zip")
-
-#unziping folder
-function Expand-ZIPFile($file, $destination)
-{
-$shell = new-object -com shell.application
-$zip = $shell.NameSpace($file)
-foreach($item in $zip.items())
-{
-$shell.Namespace($destination).copyhere($item)
-}
-}
-Expand-ZIPFile -File "C:\MCW\MCW-Innovate-and-modernize-apps-with-Data-and-AI.zip" -Destination "C:\"
 
 #Assign Packages to Install
 choco install vscode
@@ -77,9 +57,6 @@ $WebClient.DownloadFile("https://experienceazure.blob.core.windows.net/templates
 
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://experienceazure.blob.core.windows.net/templates/innovate-and-modernize-apps-with-data-and-ai/scripts/logontask.ps1","C:\Packages\logontask.ps1")
-
-New-Item -ItemType directory -Path "C:\Temp\IoT Edge"
-New-Item -ItemType directory -Path "C:\Temp\Azure Functions"
 
 $securePassword = $AzurePassword | ConvertTo-SecureString -AsPlainText -Force
 $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $AzureUsername, $securePassword
@@ -94,42 +71,6 @@ InstallAzPowerShellModule
 
 #Install synapse modules
 Install-PackageProvider NuGet -Force
-Install-Module -Name Az.Synapse -RequiredVersion 0.3.0 -AllowClobber -Force
-
-sleep 5
-Import-Module Az.Synapse
-
-. C:\LabFiles\AzureCreds.ps1
-
-Connect-AzAccount -Credential $cred | Out-Null
-
-# Template deployment
-$resourceGroupName = (Get-AzResourceGroup | Where-Object { $_.ResourceGroupName -like "*modernize-app-*" }).ResourceGroupName
-$deploymentId =  (Get-AzResourceGroup -Name $resourceGroupName).Tags["DeploymentId"]
-
-$url = "https://experienceazure.blob.core.windows.net/templates/innovate-and-modernize-apps-with-data-and-ai/deploy-01.parameters.json"
-$output = "c:\LabFiles\parameters.json";
-$wclient = New-Object System.Net.WebClient;
-$wclient.CachePolicy = new-object System.Net.Cache.RequestCachePolicy([System.Net.Cache.RequestCacheLevel]::NoCacheNoStore);
-$wclient.Headers.Add("Cache-Control", "no-cache");
-$wclient.DownloadFile($url, $output)
-(Get-Content -Path "c:\LabFiles\parameters.json") | ForEach-Object {$_ -Replace "GET-AZUSER-PASSWORD", "$AzurePassword"} | Set-Content -Path "c:\LabFiles\parameters.json"
-(Get-Content -Path "c:\LabFiles\parameters.json") | ForEach-Object {$_ -Replace "GET-DEPLOYMENT-ID", "$DeploymentID"} | Set-Content -Path "c:\LabFiles\parameters.json"
-(Get-Content -Path "c:\LabFiles\parameters.json") | ForEach-Object {$_ -Replace "GET-AZUSER-UPN", "$AzureUserName "} | Set-Content -Path "c:\LabFiles\parameters.json"
-(Get-Content -Path "c:\LabFiles\parameters.json") | ForEach-Object {$_ -Replace "GET-AZUSER-OBJECTID", "$azuserobjectid"} | Set-Content -Path "c:\LabFiles\parameters.json"
-
- 
-
-Write-Host "Starting main deployment." -ForegroundColor Green -Verbose
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri "https://experienceazure.blob.core.windows.net/templates/innovate-and-modernize-apps-with-data-and-ai/deploy-01.json" -TemplateParameterFile "c:\LabFiles\parameters.json"
-
-New-AzRoleAssignment -ResourceGroupName $resourceGroupName -ErrorAction Ignore -ObjectId "37548b2e-e5ab-4d2b-b0da-4d812f56c30e" -RoleDefinitionName "Owner"
-
-#Role assignment on synapse workspace for AAD group
-$workspacename = "asaworkspace$($deploymentId)"
-New-AzSynapseRoleAssignment -WorkspaceName $workspacename -RoleDefinitionId "6e4bf58a-b8e1-4cc3-bbf9-d73143322b78" -ObjectId "37548b2e-e5ab-4d2b-b0da-4d812f56c30e"
-New-AzSynapseRoleAssignment -WorkspaceName $workspacename -RoleDefinitionId "7af0c69a-a548-47d6-aea3-d00e69bd83aa" -ObjectId "37548b2e-e5ab-4d2b-b0da-4d812f56c30e"
-New-AzSynapseRoleAssignment -WorkspaceName $workspacename -RoleDefinitionId "c3a6d2f1-a26f-4810-9b0f-591308d5cbf1" -ObjectId "37548b2e-e5ab-4d2b-b0da-4d812f56c30e"
 
 sleep 5
 
